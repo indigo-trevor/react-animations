970d345287cadc608cd9909842e0cfd9a86c1867
Trevor Robinson <trevor.robinson@indigoslate.com>
2017-10-27 13:57:59 -0700

    set up project

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "./css/styles.scss";
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./components/app";
+
+const renderApp = Component =>
+	render(
+		<Router>
+			<Component />
+		</Router>,
+		document.getElementById("root")
+	);
+
+renderApp(App);
+
+if (module.hot) module.hot.accept("./components/app", () => renderApp(App));
